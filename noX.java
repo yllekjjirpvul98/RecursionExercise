809c87c8f064933ecac9b346313d4bb5cef00381
Ching Man Kelly Yung <32491087+yllekjjirpvul98@users.noreply.github.com>
2018-03-10 03:21:26 +0000

    Update noX.java

--- noX.java
+++ noX.java
@@ -1,13 +1,13 @@
-/*
-
-Given a string, compute recursively a new string where all the 'x' chars have been removed.
 
 
-noX("xaxb") → "ab"
-noX("abc") → "abc"
-noX("xx") → ""
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
 
-*/
+
+// noX("xaxb") → "ab"
+// noX("abc") → "abc"
+// noX("xx") → ""
+
+
 
 public String noX(String str) {
   if (str.equals("x")) return "";
